aca0b56e304a15800780973c5cbd4d5ef08294b7
sam-saenz <samsaenz31@gmail.com>
2023-04-16 21:23:43 -0700

    Completed weekly assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,39 +14,75 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
-// Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
-// Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
-// Expected output: "3cc3ntr1c"
-
+describe('codeString', () => {
+    it('Will take in a string, anad return it with certain characters replaced', () => {
+        const secretCodeWord1 = "Lackadaisical"
+        // Expected output: "L4ck4d41s1c4l"
+        const secretCodeWord2 = "Gobbledygook"
+        // Expected output: "G0bbl3dyg00k"
+        const secretCodeWord3 = "Eccentric"
+        // Expected output: "3cc3ntr1c"
+        expect(codeString(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+        expect(codeString(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+        expect(codeString(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+})
 // b) Create the function that makes the test pass.
+
+// I will write a function codeString that takes in a string, and uses a series of string.replace methods and the //g global syntax to replace all instances of a, A, e, E, i, I, o, and O with 4, 3, 1, and 0. I will return the output of that statement.
+
+let codeString = (string) => {
+    return string.replace(/a/g, "4").replace(/e/g, "3").replace(/i/g, "1").replace(/o/g, "0").replace(/A/g, "4").replace(/E/g, "3").replace(/I/g, "1").replace(/O/g, "0")
+}
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
-const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
-
-const letterA = "a"
-// Expected output: ["Mango", "Apricot", "Peach"]
-const letterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
-
+describe('filterArray', () => {
+    it('Will take in an array and a letter, and filter any words not containing the letter', () => {
+        const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
+        const letterA = "a"
+        // Expected output: ["Mango", "Apricot", "Peach"]
+        const letterE = "e"
+        // Expected output: ["Cherry", "Blueberry", "Peach"]
+        expect(filterArray(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
+        expect(filterArray(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+})
 // b) Create the function that makes the test pass.
+
+// I will create a function call filterArray that takes in an array and a string as an argument, then use .filter to get rid of any strings in the array that do not include the given letter in either uppercase or lowercase.
+
+let filterArray = (array, letter) => {
+    return array.filter((value) => value.includes(letter.toLowerCase()) || value.includes(letter.toUpperCase()))
+}
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
-// Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
-// Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
-// Expected output: false
-const hand4 = [7, 2, 7, 2, 7]
-// Expected output: true
-
+describe('fullHouse', () => {
+    it('Will take in an array of numbers, check if they make a full house, and return the result', () => {
+        const hand1 = [5, 5, 5, 3, 3]
+        // Expected output: true
+        const hand2 = [5, 5, 3, 3, 4]
+        // Expected output: false
+        const hand3 = [5, 5, 5, 5, 4]
+        // Expected output: false
+        const hand4 = [7, 2, 7, 2, 7]
+        // Expected output: true
+        expect(fullHouse(hand1)).toEqual(true)
+        expect(fullHouse(hand2)).toEqual(false)
+        expect(fullHouse(hand3)).toEqual(false)
+        expect(fullHouse(hand4)).toEqual(true)
+    })
+})
 // b) Create the function that makes the test pass.
+
+// I will make a function called fullHouse that takes in an array of 5 numbers, then check if the sorted array has different numbers at index 0 and 4(to make sure that it isnt 5 of a kind), and if the number at index 0 and 2 are equal(meaning the first three numbers are the same), and 3 and 4 are equal, OR if index 0 and 1 are equal and index 2 and 4 are equal(meaning the last three numbers are the same). 
+
+let fullHouse = (array) => {
+    array = array.sort()
+    return (array[0] !== array[4] && (array[0] === array[2] && array[3] === array[4]) || (array[0] === array[1] && array[2] === array[4]))
+}
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -10,6 +10,16 @@ letter_o = 'o'
 letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
+# I will make a function called find_letter, that has parameters array and letter, which uses the select method on our array.
+# The select method will search each index of the array, and select only those entires which .include? the given letter.
+
+def find_letter array, letter
+    array.select { |word| word.include? letter}
+end
+
+p find_letter(beverages_array, letter_o)
+p find_letter(beverages_array, letter_t)
+
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
@@ -17,12 +27,45 @@ letter_t = 't'
 us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
+# I will create a method that takes in the hash, and extracts the values with .value. Then, I will usse .flatten to get rid of the nested arrays, turning it into one array with each value. Finally, I will .sort.
+
+def unload_hash hash
+    hash.values.flatten.sort
+end
+
+p unload_hash(us_states)
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
+# I will create the class with one expected argument for the model, and a method that uses string interpolation to return the bike's info.
 
+class Bike
+    attr_accessor :model, :wheels, :current_speed
+    def initialize(model)
+        @model = model
+        @wheels = 2
+        @current_speed = 0
+    end
+    def bike_info
+        "The #{@model} bike has #{@wheels} wheels and is going #{@current_speed} mph."
+    end
+    def pedal_faster num
+        @current_speed += num
+        p "New speed is #{@current_speed} mph."
+    end
+    def brake num
+        @current_speed -= num
+        if @current_speed < 0 
+            @current_speed = 0
+        end
+        p "New speed is #{@current_speed} mph."
+    end
+end
+
+my_bike = Bike.new('Trek')
+p my_bike.bike_info
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
@@ -30,3 +73,11 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output example: my_bike.pedal_faster(18) => 28
 # Expected output example: my_bike.brake(5) => 23
 # Expected output example: my_bike.brake(25) => 0
+
+# I will create the method pedal_faster, which takes in a number, and adds it to current_speed, then prints the new speed.
+# For brake, I will subtract the given num from current_speed, then set current_speed to 0 if it is negative. 
+
+my_bike.pedal_faster(10)
+my_bike.pedal_faster(18)
+my_bike.brake(5)
+my_bike.brake(25)
